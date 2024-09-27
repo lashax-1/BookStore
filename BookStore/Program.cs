1546965dea46e5b00ef4559e8809c832eb3dc5e2
vsaginadze <vakhtang_saginade@protonmail.com>
2024-09-27 21:42:24 +0400

    inital commit

---
+++ BookStore/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ Book Store Application
+
+* can display books
+* users can register
+* users can append book in crate
+* (optional) users can add books in whishlist
+* users can buy books
+
+* Admin should be able to add books into the system
+* Admin should be able to edit the price of books, but not other info.
+* Admin should be able to LOAD books from source
+* 
+* Source should be something like API
+* Source should display all the books that are available at the current time.
+* (optional) If certain book is about to run out users should know about it.
+* 
+* (optional) Apply student discount
+* (optional) Ability to add your personal ID and get student discount if your ID says student
+* 
+* 
+* STORAGE
+* ---
+* books
+*   title: benjamin franklin: an american life
+*   auhtor: walter isaacson
+*   isbn: 
+*   price: $18.54
+*   
+*   
+* BOOKSTORE
+* ---
+* sign in page
+* registration page
+* books page (10 books max and you can move to next page)
+* individual book page
+*   purchase page
+* sign out option
+* 
+* ADMIN
+* ---
+* admin should be able to load books from the source
+* admin should be able to edit price of the book
+* admin should be able to delete book from the store
+* admin should be able to view all the money from bookstore
+* 
+* build stuff with scalability in mind
+* different items (books and let's say magazines)
+* 
+* 
+* book
+* 
+* 
+* 
+* 
+ */
